be25b49f48bd53cf1b7bb87b10ab50cf812a8083
mildShaman <rickjason1326@gmail.com>
2024-06-14 15:48:30 +0300

    more gestures for testing

--- server/src/main/java/com/example/clientservertesttask/server/model/gestrues/InMemoryGesturesRepository.kt
+++ server/src/main/java/com/example/clientservertesttask/server/model/gestrues/InMemoryGesturesRepository.kt
@@ -9,7 +9,11 @@ class InMemoryGesturesRepository @Inject constructor(): GesturesRepository {
     override fun getGestures(): List<Gesture> {
         return listOf(
             Gesture(50f, 50f, 200f, 50f, 500),
-            Gesture(50f, 50f, 50f, 200f, 600)
+            Gesture(50f, 50f, 50f, 1000f, 600),
+            Gesture(400f, 1200f, 400f, 100f, 500),
+            Gesture(400f, 1800f, 400f, 100f, 500),
+            Gesture(400f, 1800f, 400f, 100f, 500),
+            Gesture(400f, 400f, 400f, 1800f, 100)
         )
     }
 }
